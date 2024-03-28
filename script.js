ba7730e5f3e53b17e1074c6aefba7835039ffd4c
Elizaveta <e.nesterovich007@gmail.com>
2024-03-29 00:11:44 +0300

    🤐

---
+++ script.js
@@ -0,0 +1,13 @@
+// const button = document.querySelectorAll('.button');
+
+// button.addEventListener('click', function (e) {
+//     e.preventDefault();
+
+//     const href = e.target.getAttribute('href');
+//     //2.Определить таргет элемент
+//     if (e.target.classList.contains('nav__link') && href !== '#') {
+//       // const href = e.target.getAttribute('href');
+
+//       document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+//     }
+//   });
